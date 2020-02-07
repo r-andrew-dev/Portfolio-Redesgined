1c2818e1ba19a9867e40e51599874664f4d992c5
Rachael Andrew <r.andrew.dev@gmail.com>
2020-02-06 23:47:39 -0600

    inserted images and gifs for work, added transitions on hover, added size change and gif on click

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,9 +1,29 @@
 $(".button-primary").on("click", function() {
 
     $(".button-primary").text("Reach Out");
-    $("#bio, #picture").fadeOut("slow", function() {
-        $("#test").fadeIn("slow");
 
+    $("#bio, #picture").fadeOut("slow", function() {
+        $(".hidden1").fadeIn("slow");
     });
 
+    divClone = $("div.work-div").clone(true)
+
 });
+
+    $(".work").on("click", function() {
+
+        $(this).css({"width": "600px", "margin": "30%"})
+        $(this).attr("src", $(this).attr("data-attr"));
+        $(this).removeClass("work")
+        $(this).css("opacity", "1");
+        $(".work").hide();
+        $(".button-primary").text("Back to Work");
+        $(".button-primary").addClass("One");
+
+    })
+
+    $(".One").on("click", function() {
+
+        $(divClone.clone(true)).appendTo(".hidden");
+        
+    });
