fab0fb56bbaf067d0b0ab17874af73a10756c6fa
Rachael Andrew <r.andrew.dev@gmail.com>
2020-02-10 18:49:21 -0600

    styled work page, almost working at 100%, began creating page/div

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,29 +1,48 @@
-$(".button-primary").on("click", function() {
 
-    $(".button-primary").text("Reach Out");
+
+$(".One").on("click", function() {
+    $(".One").addClass("Two");
+    $(".Two").removeClass("One");
+    $(".Two").text("Reach Out");
+
 
     $("#bio, #picture").fadeOut("slow", function() {
         $(".hidden1").fadeIn("slow");
     });
 
-    divClone = $("div.work-div").clone(true)
-
 });
 
     $(".work").on("click", function() {
-
-        $(this).css({"width": "600px", "margin": "30%"})
+        $(this).css({"width": "600px", "margin": "10% auto", "opacity": "1", "display": "block"});
+        $(this).parent().removeClass("six");
+        $(this).parent().addClass("twelve current");
+        $(this).addClass("current-img")
         $(this).attr("src", $(this).attr("data-attr"));
-        $(this).removeClass("work")
+        $(this).removeClass("work");
         $(this).css("opacity", "1");
         $(".work").hide();
-        $(".button-primary").text("Back to Work");
-        $(".button-primary").addClass("One");
+        $(".Two").addClass("Three");
+        $(".Three").removeClass("Two")
+        $(".Three").text("Back to Work");
+        $(".Site").css("display", "inline");
+        $(".Site").attr("href", $(this).data("site"));
+    });
 
+    $("#button-holder").on("click", ".Two", function() {
+        $(".container").css("display", "none");
+        $("#contact").css("display", "block");
     })
 
-    $(".One").on("click", function() {
-
-        $(divClone.clone(true)).appendTo(".hidden");
+    $("#button-holder").on("click", ".Three", function() {
+        console.log("clicked");
+        $(".work").show();
+        $(".current-img").attr("src", $(".current-img").data("main"));
+        $(".current-img").css({"width": "300px", "margin": "3%", "opacity":"0.6", "display":"inline"});
+        $(".current-img").addClass("work");
+        $(".current-img").parent().removeClass("twelve current");
+        $(".current-img").parent().addClass("six");
+        $(".u-max-full-width").removeClass("current-img");
+        $("#odd-row").children().attr("class", "twelve columns")
+        $(".Two").text("Reach Out");
         
     });
